6fb32791825fc2ca55c3d29d4b0d104066200386
topaccina <54760952+topaccina@users.noreply.github.com>
2023-01-22 10:14:50 +0100

    update sample dataset and contact tab

--- main_prjOverview.py
+++ main_prjOverview.py
@@ -3,7 +3,11 @@ from dash import Dash, html, dcc, Input, Output, dash_table, ctx, no_update
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
+# import numpy as np
+# import datetime as dt
 import plotly.express as px
+
 ###############################################################################
 # import data
 df = pd.read_csv('./data/CFP_Alldata.csv')
@@ -13,43 +17,44 @@ df_dload = df[['N', 'ROUND', 'TYPE', 'TITLE',
 # data prep
 ###############################################################################
 # card section data
-totalRound = len(df['round'].unique())
-lastRoundDate = pd.to_datetime(df['date'].astype('datetime64[ns]')).dt.strftime(
+totalRound = len(df['ROUND'].unique())
+lastRoundDate = pd.to_datetime(df['DATE'].astype('datetime64[ns]')).dt.strftime(
     '%Y-%m').sort_values(ascending=False)[0]
-totalDFIRequested = df[(df.category == 'cfp')]['dfiAmount'].sum()
-totalDFIAssigned = df[(df.currentResult == 'Approved') &
-                      (df.category == 'cfp')]['dfiAmount'].sum()
+totalDFIRequested = df[(df.TYPE == 'cfp')]['DFIREQ'].sum()
+totalDFIAssigned = df[(df.RESULT == 'Approved') &
+                      (df.TYPE == 'cfp')]['DFIREQ'].sum()
 totalDFIAssignedPct = round(100*totalDFIAssigned/totalDFIRequested, 1)
-totalCFP = df.category.value_counts().loc['cfp']
-totalCFPApproved = df[(df.currentResult == 'Approved') &
-                      (df.category == 'cfp')].shape[0]
+totalCFP = df.TYPE.value_counts().loc['cfp']
+totalCFPApproved = df[(df.RESULT == 'Approved') &
+                      (df.TYPE == 'cfp')].shape[0]
 totalCFPApprovedPct = round(100*totalCFPApproved/totalCFP, 1)
-totalDFIP = df.category.value_counts().loc['dfip']
-totalDFIPApproved = df[(df.currentResult == 'Approved')
-                       & (df.category == 'dfip')].shape[0]
+totalDFIP = df.TYPE.value_counts().loc['dfip']
+totalDFIPApproved = df[(df.RESULT == 'Approved')
+                       & (df.TYPE == 'dfip')].shape[0]
 totalDFIPApprovedPct = round(100*totalDFIPApproved/totalDFIP, 1)
 ############################################################################
 # category plots data
-df_cfp_cat = df[(df.currentResult == 'Approved') & (
-    df.category == 'cfp')]['CATEGORY'].value_counts().reset_index()
+df_cfp_cat = df[(df.RESULT == 'Approved') & (
+    df.TYPE == 'cfp')]['CATEGORY'].value_counts().reset_index()
 df_cfp_cat.columns = ['category', 'count']
 #############################################################################
 # dfi assegned/year data
-df_dfi_yyyy = df[(df.currentResult == 'Approved')].groupby(df.date.astype(
-    'datetime64[ns]').dt.strftime('%Y'))['dfiAmount'].sum().reset_index()
+df_dfi_yyyy = df[(df.RESULT == 'Approved')].groupby(df.DATE.astype(
+    'datetime64[ns]').dt.strftime('%Y'))['DFIREQ'].sum().reset_index()
 ############################################################################
 # dfi assegned/year/round data ---->NOT USED!!!!!!!
-df_dfi_yyyy_round = df[(df.currentResult == 'Approved')].groupby([df.date.astype(
-    'datetime64[ns]').dt.strftime('%Y'), 'round'])['dfiAmount'].sum().reset_index()
+df_dfi_yyyy_round = df[(df.RESULT == 'Approved')].groupby([df.DATE.astype(
+    'datetime64[ns]').dt.strftime('%Y'), 'ROUND'])['DFIREQ'].sum().reset_index()
 #############################################################################
 # dfi per cfp data
-df_dfi_cfp = df[(df.currentResult == 'Approved') & (
-    df.category == 'cfp')][['number', 'dfiAmount']]
-df_dfi_cfp_sorted = df_dfi_cfp.sort_values(by='dfiAmount', ascending=False)
+df_dfi_cfp = df[(df.RESULT == 'Approved') & (
+    df.TYPE == 'cfp')][['N', 'DFIREQ', 'TITLE']]
+df_dfi_cfp_sorted = df_dfi_cfp.sort_values(by='DFIREQ', ascending=False)
 ##############################################################################
 # masternode statistics per round
 df_masternodes_round = df.groupby(
-    by='date')['possible'].unique().apply(lambda x: x[0]).reset_index()
+    df.DATE.astype(
+        'datetime64[ns]').dt.strftime('%Y-%m'))['MASTERNODESCOUNT'].unique().apply(lambda x: x[0]).reset_index()
 ##############################################################################
 # plots
 # CFP by category pie chart
@@ -69,8 +74,8 @@ fig_2_1.update_layout(
 )
 # plots
 # DFI assigned/Year pie chart
-labels = df_dfi_yyyy['date'].unique()
-values = df_dfi_yyyy['dfiAmount'].unique()
+labels = df_dfi_yyyy['DATE'].unique()
+values = df_dfi_yyyy['DFIREQ'].unique()
 fig_3_1 = go.Figure(
     go.Pie(labels=labels, values=values, name="DFIAssingnedYear"))
 fig_3_1.update_traces(hoverinfo="label+percent+value+name", textinfo="percent+label", marker=dict(
@@ -85,12 +90,13 @@ fig_3_1.update_layout(
 )
 
 fig_2_3 = go.Figure()
-fig_3_2 = go.Figure(go.Scatter(x=df_masternodes_round.date, y=df_masternodes_round.possible, mode="markers+lines",
+fig_3_2 = go.Figure(go.Scatter(x=df_masternodes_round.DATE, y=df_masternodes_round.MASTERNODESCOUNT, mode="markers+lines",
                                name="Assigned DFI - cumulative sum",
                                line=dict(width=6, color='#FF00FF'),
                                hoverinfo='text', fill='tozeroy', marker=dict(size=12),
-                               hovertext=df_masternodes_round['date'].astype(str)+'-masternodes ' +
-                               df_masternodes_round['possible'].astype(str),
+                               hovertext=df_masternodes_round['DATE'].astype(str)+'-masternodes ' +
+                               df_masternodes_round['MASTERNODESCOUNT'].astype(
+                                   str),
                                ))
 fig_3_2.update_traces(fillcolor='rgba(255, 0, 255, 0.1)')
 fig_3_2.update_layout(title={'text': 'Masternode with Voting Rights',
@@ -193,8 +199,37 @@ def generate_modal():
     )
 
 
+# author/collaborators card
+# contactsCard = dbc.Card([
+#     dbc.Row([
+#         dbc.Col([dbc.CardImg(src='assets/me.png', top=True,
+#                 style={'max-width': '50%', 'max-height': '50%'}), ]),
+#         dbc.Col(dbc.CardBody(
+#             [html.H3([html.A(html.I(className="bi bi-linkedin "), href='https://www.linkedin.com/in/laura-lorenzi-14885a12/', target='__blank__'), " Laura L.", ], className="text-nowrap", style={'margin-bottom': '0px',   'font-size': 20,  'line-height': '0.5'}),
+#              html.H6([" Maintaner and Developer"],
+#                      style={'margin-bottom': '0px',  'font-size': 15, 'text-transform': 'uppercase'}), html.Hr(style={'margin': '20px'}),
+#              html.Div([html.A(html.I(className="bi bi-envelope mx-2"), href='mailto:myemailaddress@gmail.com', target='__blank__'),
+#                        html.A(html.I(className="bi bi-twitter"), href='mailto:myemailaddress@gmail.com', target='__blank__')], style={'margin': '0px 40px',   'font-size': 30,  'line-height': '0.5',  'display': 'flex', 'justify-content': 'space-evenly'}),
+
+#              ],))])], className="my-5",)
+contactsCard = html.Div([html.Div([html.Img(src='assets/cfpOv_ico.jpg',
+                                            id='logo-us-image',
+                                            style={'height': 'auto',
+                                                   'width': '100%',
+                                                   })], style={'margin-bottom': '0px'}),
+                         html.Div([html.H6([html.A(html.I(className="bi bi-envelope "), href='mailto:defichainproposal@gmail.com', target='__blank__'), html.A(html.I(className="bi bi-twitter "), href='https://twitter.com/DefiProposal', target='__blank__')],  style={'margin-bottom': '0px', 'font-size': 40, 'display': 'flex', 'justify-content': 'space-evenly'}),
+                                   html.Hr(
+                                       style={'margin-top': '0px', 'margin-bottom': '10px'}),
+                                   html.H6([html.A(html.I(className="bi bi-linkedin "), href='https://www.linkedin.com/in/laura-lorenzi-14885a12/', target='__blank__'), " Laura L."],
+                                           style={'margin-bottom': '0px',  'font-size': 20}),
+                                   html.H6(["Maintaner and Developer"],
+                                           style={'margin-bottom': '0px',  'font-size': 15})], ),
+                         ], className='contacts-container-flex')
+
+
 app = Dash(__name__, suppress_callback_exceptions=True, meta_tags=[
-    {"name": "viewport", "content": "width=device-width"}])
+    {"name": "viewport", "content": "width=device-width"}], external_stylesheets=[dbc.icons.BOOTSTRAP])
+#server = app.server
 
 app.layout = html.Div([
     html.Div([
@@ -203,7 +238,7 @@ app.layout = html.Div([
                 html.H3('CFP and DFIP Overview ',
                         style={'margin-bottom': '0px',   'font-size': 20,  'line-height': '0.5'}),
                 html.H6('Statistics and Projects Reporting',
-                        style={'margin-bottom': '0px',  'font-size': 15})],)
+                        style={'margin-bottom': '0px',  'font-size': 15}), ],),
         ], style={'justify-content': 'flex-start', 'flex-basis': '90%'}, id='title', ),
         html.Div([
             html.A(
@@ -287,8 +322,8 @@ app.layout = html.Div([
                                 html.Label(
                                     ['Round'], className='filter-label'),
                                 dcc.Dropdown(id='dropdown-round',
-                                             options=df['round'].unique(),
-                                             # value=df['round'].unique()[0],
+                                             options=df['ROUND'].unique(),
+                                             # value=df['ROUND'].unique()[0],
                                              placeholder='select a value',
                                              multi=False,
                                              clearable=True,
@@ -322,8 +357,8 @@ app.layout = html.Div([
                                 html.Label(
                                     ['Result'], className='filter-label'),
                                 dcc.Dropdown(id='dropdown-result',
-                                             options=df.currentResult.unique(),
-                                             # value=df.currentResult.unique()[0],
+                                             options=df.RESULT.unique(),
+                                             # value=df.RESULT.unique()[0],
                                              multi=False,
                                              clearable=True,
                                              className='filter-dropdown',
@@ -371,9 +406,9 @@ app.layout = html.Div([
                                     data=df.to_dict(
                                         'records'),
                                     columns=[{'id': d, 'name': c}
-                                             # for c in df.loc[:, ['title', 'dfiAmount', 'CATEGORY', 'REQUESTER', 'STATUS']]
+                                             # for c in df.loc[:, ['title', 'DFIREQ', 'CATEGORY', 'REQUESTER', 'STATUS']]
                                              for c, d in zip(['Title', 'Result', 'DFI Amount', 'Category', 'Requester', 'Status'], df.loc[:, [
-                                                 'title', 'currentResult', 'dfiAmount', 'CATEGORY', 'REQUESTER', 'STATUS']])
+                                                 'TITLE', 'RESULT', 'DFIREQ', 'CATEGORY', 'REQUESTER', 'STATUS']])
                                              # for c in df.columns
                                              ],
                                     #  sort_action="native",
@@ -407,7 +442,7 @@ app.layout = html.Div([
                                     style_cell_conditional=[
                                         {'if': {'column_id': ['title']},
                                          'maxWidth': '200px'},
-                                        {'if': {'column_id': ['currentResult', 'dfiAmount']},
+                                        {'if': {'column_id': ['RESULT', 'DFIREQ']},
                                          'maxWidth': '50px'},
 
                                     ],
@@ -441,8 +476,10 @@ app.layout = html.Div([
 
                     ], className='flex-display container-tab2 '),  # end container tab2 content
                 ]),  # end tab2
-        dcc.Tab(label='Coming Soon...', value='tab3', id='tab3', className='custom-tab',
+        dcc.Tab(label='Contacts', value='tab3', id='tab3', className='custom-tab',
                 selected_className="custom-tab--selected", children=[
+
+                    html.Div([contactsCard])
                 ]),
     ], className='custom-tabs', value='tab1'),
 
@@ -457,19 +494,21 @@ app.layout = html.Div([
 )
 def CFP_DFI_bar(radio_sel):
     if radio_sel == 'All':
-        x = df_dfi_cfp_sorted['number'].astype(str)
-        y = df_dfi_cfp_sorted['dfiAmount']
+        x = df_dfi_cfp_sorted['N'].astype(str)
+        y = df_dfi_cfp_sorted['DFIREQ']
+        z = df_dfi_cfp_sorted['TITLE']
     else:
-        x = df_dfi_cfp_sorted['number'].astype(str)[0: 10]
-        y = df_dfi_cfp_sorted['dfiAmount'][0: 10]
+        x = df_dfi_cfp_sorted['N'].astype(str)[0: 10]
+        y = df_dfi_cfp_sorted['DFIREQ'][0: 10]
+        z = df_dfi_cfp_sorted['TITLE'][0: 10]
 
     fig_2_3 = go.Figure(go.Bar(x=x, y=y,
                                name='Assigned DFI by CFP',
                                marker=dict(color='orange'),
                                hoverinfo='text',
-                               hovertext='CFP Id '+x+' assigned ' +
-                               y.map(
-                                   '{:,.0f}'.format).astype(str)+' DFI',
+                               hovertext=''+z  # +' assigned '  +
+                               # y.map(
+                               #    '{:,.0f}'.format).astype(str)+' DFI',
                                )
 
                         )
@@ -534,13 +573,13 @@ def update_dropdown_options(round_v, category_v, type_v, result_v, requester_v):
     dff = df.copy()
 
     if round_v is not None:
-        dff = dff[dff['round'] == round_v]
+        dff = dff[dff['ROUND'] == round_v]
     if category_v is not None:
         dff = dff[dff['CATEGORY'] == category_v]
     if type_v is not None:
         dff = dff[dff['TYPE'] == type_v]
     if result_v is not None:
-        dff = dff[dff['currentResult'] == result_v]
+        dff = dff[dff['RESULT'] == result_v]
     if requester_v is not None:
         dff = dff[dff['REQUESTER'] == requester_v]
 
@@ -554,7 +593,7 @@ def update_dropdown_options(round_v, category_v, type_v, result_v, requester_v):
         ]
 
 
-@app.callback(
+@ app.callback(
     [Output('dropdown-round', 'value'),
      Output('dropdown-category', 'value'),
      Output('dropdown-type', 'value'),
@@ -567,23 +606,29 @@ def clear_tbl_filter(n1):
     return None, None, None, None, None
 
 
-@app.callback(
+@ app.callback(
     Output("markdown", "style"),
     [
         # Input("button-selection-details", "n_clicks"),
         Input('table-cfp-dfip', 'active_cell'),
         Input("markdown_close", "n_clicks")],
 )
+# def update_click_output(button_click, active_cell, close_click):
 def update_click_output(active_cell, close_click):
+    # ctx = callback_context
+
     if ctx.triggered:
         prop_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        # print(prop_id)
+        # if prop_id == "button-selection-details":
+        #     return {"display": "block"}
         if prop_id == "table-cfp-dfip":
             return {"display": "block"}
 
     return {"display": "none"}
 
 
-@app.callback(
+@ app.callback(
 
     [Output('title-id', 'children'),
      Output('result-id', 'children'),
@@ -603,9 +648,11 @@ def testTbl(active_cell, tblData):
     if active_cell is None:
         return no_update
     selRows = active_cell["row"]
+    # print(f"row id: {selRows}")
+    # print(df.loc[selRows]['title'])
     if selRows is not None:
-        title = df.loc[selRows]['title']
-        dfiAmount = df.loc[selRows]['dfiAmount']
+        title = df.loc[selRows]['TITLE']
+        dfiAmount = df.loc[selRows]['DFIREQ']
         result = df.loc[selRows]['RESULT']
         requester = df.loc[selRows]['REQUESTER']
         category = df.loc[selRows]['CATEGORY']
@@ -629,7 +676,7 @@ def testTbl(active_cell, tblData):
         return 'Select a row for more details', '', '', '', '', '', '', '', '', ''
 
 
-@app.callback(
+@ app.callback(
     Output("download-dataframe-csv", "data"),
     Input("button-download", "n_clicks"),
     prevent_initial_call=True,
